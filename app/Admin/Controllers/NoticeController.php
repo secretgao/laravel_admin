e6adc71eacaf0182d0977c72370259862c05bdb1
kerr <kerr>
2023-06-13 19:31:25 +0800

    f

---
+++ app/Admin/Controllers/NoticeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Admin\Repositories\Notice;
+use Dcat\Admin\Form;
+use Dcat\Admin\Grid;
+use Dcat\Admin\Show;
+use Dcat\Admin\Http\Controllers\AdminController;
+
+class NoticeController extends AdminController
+{
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Grid::make(new Notice(), function (Grid $grid) {
+            $grid->column('id')->sortable();
+            $grid->column('nocite');
+            $grid->column('created_at');
+            $grid->column('updated_at')->sortable();
+
+            $grid->filter(function (Grid\Filter $filter) {
+                $filter->equal('id');
+
+            });
+        });
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     *
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        return Show::make($id, new Notice(), function (Show $show) {
+            $show->field('id');
+            $show->field('id');
+            $show->field('nocite');
+            $show->field('created_at');
+            $show->field('updated_at');
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Form::make(new Notice(), function (Form $form) {
+            $form->display('id');
+            $form->text('notice');
+
+            $form->display('created_at');
+            $form->display('updated_at');
+        });
+    }
+}
---
+++ resources/lang/zh_CN/notice.php
@@ -0,0 +1,13 @@
+<?php 
+return [
+    'labels' => [
+        'Notice' => 'Notice',
+        'notice' => 'Notice',
+    ],
+    'fields' => [
+        'id' => 'id',
+        'nocite' => 'nocite',
+    ],
+    'options' => [
+    ],
+];
