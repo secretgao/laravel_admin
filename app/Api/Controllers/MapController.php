3e3c215e74444ed8373c1e8a8a1b151049893fc3
secret <secret>
2023-06-29 14:06:33 +0800

    edit

--- app/Api/Controllers/MapController.php
+++ app/Api/Controllers/MapController.php
@@ -20,7 +20,7 @@ class MapController extends Controller
     public function index()
     {
         $data=MapMarkedLocation::query()
-            ->select(['name','contact_information','pic_url','comment','lat','lng','address'])
+            ->select(['id','name','contact_information','pic_url','comment','lat','lng','address'])
             ->get()->toArray();
 
         if ($data){
