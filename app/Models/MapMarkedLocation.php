7c55e999bcdde622817156288a3ce5d522e26a33
secret <secret>
2023-06-30 18:11:18 +0800

    edit

--- app/Models/MapMarkedLocation.php
+++ app/Models/MapMarkedLocation.php
@@ -11,6 +11,6 @@ class MapMarkedLocation extends Model
 	use HasDateTimeFormatter;
     protected $table = 'map_marked_location';
 
-    protected $fillable = ['name','contact_information', 'pic_url', 'comment', 'lat', 'lng','address'];
+    protected $fillable = ['name','contact_information', 'pic_url', 'comment', 'lat', 'lng','address','label_id'];
     
 }
